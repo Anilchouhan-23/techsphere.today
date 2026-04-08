d79de50aa097b6e93c0c8ae851a2e105f439edd7
Kishore <kishore@example.com>
2026-04-08 13:08:03 +0530

    resolve merge conflicts in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-=======
 import TopBar from "@/components/TopBar";
->>>>>>> 906b4ecf8ce36730193b01a9b71dedf6c46b115a
 import Navbar from "@/components/Navbar";
 import HeroSlider from "@/components/HeroSlider";
 import About from "@/components/About";
@@ -16,14 +13,10 @@ import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <er />
-=======
     <>
       <TopBar />
       <Navbar />
       <HeroSlider />
->>>>>>> 906b4ecf8ce36730193b01a9b71dedf6c46b115a
       <About />
       <Highlights />
       <Services />
